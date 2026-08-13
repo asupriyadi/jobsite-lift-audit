2fd2eac67e733fcb70c12a8d8545daca6cca6026
asupriyadi <61440415+asupriyadi@users.noreply.github.com>
2026-08-13 10:43:25 +0000

    Fix CORS middleware settings in server.py

--- backend/server.py
+++ backend/server.py
@@ -1129,8 +1129,8 @@ async def shutdown():
 app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
